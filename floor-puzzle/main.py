7385518ededa5256062975d7202256a65df5eeb2
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2021-10-06 09:16:57 +0200

    refactor: correct spelling

--- floor-puzzle/main.py
+++ floor-puzzle/main.py
@@ -1,24 +1,24 @@
 from itertools import permutations
 
 
-def is_valid(combination):
+def is_valid(permutation):
     # Loes woont niet op de bovenste verdieping.
     # Niels woont niet op de bovenste verdieping.
-    if combination[4] == 'L' or combination[4] == 'N':
+    if permutation[4] == 'L' or permutation[4] == 'N':
         return False
 
     # Marja woont niet op de begane grond.
     # Niels woont niet op de begane grond.
-    if combination[0] == 'M' or combination[0] == 'N':
+    if permutation[0] == 'M' or permutation[0] == 'N':
         return False
 
     # Erik woont tenminste één verdieping hoger dan Marja.
-    e, m = combination.index('E'), combination.index('M')
+    e, m = permutation.index('E'), permutation.index('M')
     if (m + 1) > e:
         return False
 
     # Joep woont niet op een verdieping één hoger of lager dan Niels.
-    j, n = combination.index('J'), combination.index('N')
+    j, n = permutation.index('J'), permutation.index('N')
     if (j + 1 == n) or (j - 1 == n):
         return False
 
@@ -33,11 +33,11 @@ def solve():
     # Solve the floor puzzle.
     people = ('L', 'M', 'N', 'E', 'J')
 
-    # Walk through every possible combination.
-    for combination in list(permutations(people)):
+    # Walk through every possible permutation.
+    for permutation in list(permutations(people)):
 
-        if is_valid(combination):
-            print('Valid:', combination)
+        if is_valid(permutation):
+            print('Valid:', permutation)
 
 
 if __name__ == '__main__':
